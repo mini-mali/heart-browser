e1b31a8fb52976dcd18385ba379d7c3f657aae4f
František Rapčák (CS) <Frankie.Rapcak@gendigital.com>
2024-10-19 13:08:28 +0200

    vite config: make Download page part of the production build

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 // vite.config.js
 import { defineConfig } from 'vite'
+import { resolve } from 'path';
 
 export default defineConfig({
 	base: './', // This ensures assets have relative paths
 	build: {
 		outDir: 'dist', // output directory
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, 'index.html'),
+				download: resolve(__dirname, 'download.html'),
+			},
+		},
 	}
 })
